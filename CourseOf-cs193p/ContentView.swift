67a3d0d07d138219c29e9b421c96069b12116942
Bond-JH <838629462@qq.com>
2025-07-20 21:48:17 +0800

    Initial Commit

---
+++ CourseOf-cs193p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CourseOf-cs193p
+//
+//  Created by 杨家豪 on 2025/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CourseOf-cs193p/CourseOf_cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseOf_cs193pApp.swift
+//  CourseOf-cs193p
+//
+//  Created by 杨家豪 on 2025/7/20.
+//
+
+import SwiftUI
+
+@main
+struct CourseOf_cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
